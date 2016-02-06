4f141af8f2396bb37bcc4b033712bea6b7c47b3c
Alexander von Weiss <alexander.weiss@rebuy.de>
2016-02-06 21:39:39 +0100

    add jspm support

--- cacheDependencyManagers/cacheDependencyManager.js
+++ cacheDependencyManagers/cacheDependencyManager.js
@@ -8,6 +8,7 @@ var which = require('which');
 var tar = require('tar');
 var fsNode = require('fs');
 var fstream = require('fstream');
+var md5 = require('md5');
 
 
 function CacheDependencyManager (config) {
@@ -18,6 +19,14 @@ function CacheDependencyManager (config) {
 // returns a normalized absolute path
 var getAbsolutePath = function (relativePath) {
   return path.resolve(process.cwd(), relativePath);
+};
+
+var getFileBackupPath = function (installedDirectory) {
+  return path.join(installedDirectory, '.npm-cache');
+};
+
+var getFileBackupFilename = function (file) {
+  return path.basename(file) + '_' + md5(file);
 };
 
 CacheDependencyManager.prototype.cacheLogInfo = function (message) {
@@ -43,16 +52,47 @@ CacheDependencyManager.prototype.installDependencies = function () {
   return error;
 };
 
+CacheDependencyManager.prototype.backupFile = function (backupPath, file) {
+  var sourceFile = getAbsolutePath(file);
+  var backupFilename = getFileBackupFilename(file);
+  var backupFile = path.join(backupPath, backupFilename);
+  if (!fs.existsSync(sourceFile)) {
+    this.cacheLogError('backup file [file not found]:' + file);
+    return;
+  }
+
+  fs.mkdirsSync(backupPath);
+  fs.copySync(sourceFile, backupFile);
+  this.cacheLogInfo('backup file: ' + file);
+};
+
+CacheDependencyManager.prototype.restoreFile = function (backupPath, file) {
+  var sourceFile = getAbsolutePath(file);
+  var backupFilename = getFileBackupFilename(file);
+  var backupFile = path.join(backupPath, backupFilename);
+  if (!fs.existsSync(backupFile)) {
+    this.cacheLogError('restore file [file not found]:' + file);
+    return;
+  }
+
+  fs.copySync(backupFile, sourceFile);
+  this.cacheLogInfo('restore file: ' + file);
+};
 
 CacheDependencyManager.prototype.archiveDependencies = function (cacheDirectory, cachePath, callback) {
   var self = this;
   var error = null;
   var installedDirectory = getAbsolutePath(this.config.installDirectory);
+  var fileBackupDirectory = getFileBackupPath(installedDirectory);
   this.cacheLogInfo('archiving dependencies from ' + installedDirectory);
 
   if (!fs.existsSync(installedDirectory)) {
     this.cacheLogInfo('skipping archive. Install directory does not exist.');
     return error;
+  }
+
+  if (this.config.addToArchiveAndRestore) {
+    this.backupFile(fileBackupDirectory, this.config.addToArchiveAndRestore);
   }
 
   // Make sure cache directory is created
@@ -63,10 +103,18 @@ CacheDependencyManager.prototype.archiveDependencies = function (cacheDirectory,
   function onError(err) {
     error = 'error tar-ing ' + installedDirectory + ' :' + err;
     self.cacheLogError(error);
+    onFinally();
   }
 
   function onEnd() {
     self.cacheLogInfo('installed and archived dependencies');
+    onFinally();
+  }
+
+  function onFinally() {
+    if (fs.existsSync(fileBackupDirectory)) {
+      fs.removeSync(fileBackupDirectory);
+    }
   }
 
   var packer = tar.Pack({ noProprietary: true })
@@ -83,6 +131,8 @@ CacheDependencyManager.prototype.extractDependencies = function (cachePath) {
   var self = this;
   var error = null;
   var installDirectory = getAbsolutePath(this.config.installDirectory);
+  var fileBackupDirectory = getFileBackupPath(installDirectory);
+  var targetPath = path.dirname(installDirectory);
   this.cacheLogInfo('clearing installed dependencies at ' + installDirectory);
   fs.removeSync(installDirectory);
   this.cacheLogInfo('...cleared');
@@ -93,10 +143,14 @@ CacheDependencyManager.prototype.extractDependencies = function (cachePath) {
     self.cacheLogError(error);
   }
   function onEnd() {
+    if (self.config.addToArchiveAndRestore) {
+      self.restoreFile(fileBackupDirectory, self.config.addToArchiveAndRestore);
+      fs.removeSync(fileBackupDirectory);
+    }
     self.cacheLogInfo('done extracting');
   }
 
-  var extractor = tar.Extract({path: process.cwd()})
+  var extractor = tar.Extract({path: targetPath})
                      .on('error', onError)
                      .on('end', onEnd);
 
